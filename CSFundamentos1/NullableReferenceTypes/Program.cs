d2f7312ba2a325381685848a9df7f748c1ca5e9e
Rogerio Lara <rogerioo.lara@hotmail.com>
2023-08-15 00:19:44 -0300

    Precedencia / Nullable reference types

---
+++ CSFundamentos1/NullableReferenceTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("## Nullable Reference Types ##\n");
+
+// quando o tipo de referência não tem nenhuma referência, retorna null
+
+// recurso de tempo de compilação, avisa quando o código pode levar a excessões de referencia nula mas roda o código
+
+// sua finalidade é minimizar a chance do erro System.NullReferenceException
+
+// o alerta é ativado quando o element <Nullable> no projeto é definido como enable;
+// <nullable>enable</nullable>
+
+string? nome = null;
+//como acessar sem correr o risco de causar erro, se não for nulo o método é executado, se for null o resultado é null
+Console.WriteLine(nome?.ToUpper());
+
+// é possível também utilizar dessa forma:
+string name = "";
+Console.WriteLine(name?.ToUpper()); //null conditionalo operator
+
--- CSFundamentos1/PrecedenciaAssociatividade/Program.cs
+++ CSFundamentos1/PrecedenciaAssociatividade/Program.cs
@@ -20,3 +20,14 @@ bool c = !(9 != 8) && (5 >= 7 || 8 >= 6);
 Console.WriteLine(c);
 // FALSE && TRUE
 // FALSE
+
+//ordem dos operadores -> verificar tabela site microsoft
+int n1 = 5, n2 = 6, n3 = 4;
+int r = --n1 * n2 - ++n3;
+Console.WriteLine(r);
+// os incrementos precedem a multiplicação - resultado 19
+
+//nesse caso o resultado será 3 (associatividade) -> o sinal de igualdade anda da direita para a esquerda
+int x1 = 5, x2 = 6, x3 = 3;
+int r2 = x1 = x2 = x3;
+Console.WriteLine(r2);
