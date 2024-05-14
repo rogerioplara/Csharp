666c1f8cd3c6742e2485f286441cf8050458eef2
Rogerio Peres Lara <rogerio.lara@db1.com.br>
2024-05-14 14:26:08 -0300

    Fim do cap básicos

---
+++ CSFundamentos1/Break_Continue/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("## Break e Continue ##");
+
+int n = 1;
+for (int i = 0; i < 10; i++)
+{
+    if (i == 5) continue;
+
+    if (i > 8) break;
+
+    Console.WriteLine(i);
+}
+Console.WriteLine("\nFim do processamento...");
+Console.ReadKey();
---
+++ CSFundamentos1/Exercicios/Program.cs
@@ -0,0 +1,53 @@
+﻿void Ex1()
+{
+    int[] nums = new int[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        Console.WriteLine($"Número #{i + 1}: ");
+        nums[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int max = nums.Max();
+
+    Console.WriteLine($"{max} é o maior.");
+}
+
+//Ex1();
+
+void Ex3()
+{
+    int i1 = 0;
+    int soma1 = 0;
+    while (i1 <= 10)
+    {
+        soma1 += i1;
+        i1++;
+    }
+    Console.WriteLine(soma1);
+}
+
+//Ex3();
+
+void Ex4()
+{
+    Console.Write("Digite um número de 1 a 10 ou 0 para sair: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    while (num is not 0)
+    {
+        if (num > 0)
+        {
+            Console.WriteLine($"\nNúmero selecionado: {num}");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num * i}");
+            }
+        }
+
+        Console.Write("\nDigite um número de 1 a 10 ou 0 para sair: ");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+Ex4();
